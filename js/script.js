b172880c8be89aab2aed5adc40a975bfd7d435a3
Adriana Fernandes <adrianafernandes.af@gmail.com>
2019-10-02 16:42:45 -0300

    novo commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-const botaoAdd = document.getElementById("ativaBotao");;
+const botaoAdd = document.getElementById("ativaBotao");
+
 const lista = document.getElementById("lista");
 
 const erro = document.querySelector (".meu-erro");
 
-botaoAdd.addEventListener("click", function (evento) {
+const btnFeito = document.querySelector (".bnt1");
+
+const btnExcluir = document.querySelector (".bnt2");
+
+botaoAdd.addEventListener("click", function(evento) {
     evento.preventDefault();
 
-    console.log('botao clicado');
+    // console.log('botao clicado');
 
     const inputUsuario = document.getElementById("novaTarefa");
 
     let mensagem = inputUsuario.value;
 
-    if (mensagem.trim() === "") {
-        //monstrar erro
+    if (mensagem.trim() === ""){
+        
         erro.textContent = "Informe a nova tarefa";
      
     } else {
@@ -22,24 +27,81 @@ botaoAdd.addEventListener("click", function (evento) {
         let novoItem = document.createElement("div");
         lista.appendChild(novoItem);
         novoItem.classList.add("novoItem");
+        novoItem.setAttribute("class", "novoItem");
+
+        novoItem.setAttribute("draggable", "true");
+
+        // novoItem.setAttribute("ondragstart", "dragStart(event)");
+        // novoItem.setAttribute("ondragover", "overAction(event)");
+        // novoItem.setAttribute("ondrop", "dropAction(event)");
+
                     
         let tarefa = document.createElement("p");
-        tarefa.textContent = mensagem;        novoItem.appendChild(tarefa);
+        tarefa.textContent = mensagem;        
+        novoItem.appendChild(tarefa);
         tarefa.classList.add("tarefaColor");
-        
-        tarefa.addEventListener("click", function() {            if(tarefa.classList.contains("feito")) {                tarefa.classList.remove("feito")            } else {
+        tarefa.setAttribute("draggable", "true");
+
+        tarefa.addEventListener("click", function() {            
+            if(tarefa.classList.contains("feito")) {                
+                tarefa.classList.remove("feito")            
+            } else {
                 tarefa.classList.add("feito");
             }
         })
-             
+         
         let xDesfazer = document.createElement("span");    
         xDesfazer.textContent = "x";
         novoItem.appendChild(xDesfazer);
+        xDesfazer.setAttribute("draggable", "true");
 
         xDesfazer.addEventListener("click", function() {
             lista.removeChild(novoItem);
+        })    
+
+        btnFeito.addEventListener ("click", function (evento) {
+            evento.preventDefault();
+
+            if(tarefa.classList.contains("feito")) {                
+                tarefa.classList.remove("feito")            
+            } else {
+                tarefa.classList.add("feito");
+            }
         })
-        
+
+        btnExcluir.addEventListener("click", function (evento) {
+            evento.preventDefault();
+            lista.removeChild(novoItem);
+            })
+      
     }  
     inputUsuario.value = "";
+
 });
+
+// function dragStart(event) {
+//     event.dataTransfer.setData("text/plain", event.target.className);
+//     console.log('event.target.className', event.target.className);
+// }
+
+// function overAction(event){
+//     event.preventDefault();
+// }
+
+// function dropAction(event){
+//     event.preventDefault();
+//     const dados = event.dataTransfer.getData("text/plain");
+//     const elemento = document.getElementsByClassName(dados);
+//     try {
+//         event.target.appendChild(elemento);
+//     }
+//     catch(error) {
+//         console.error("não foi possivel completar o drop");
+//     }
+//     event.target.appendChild(elemento);
+//     event.stopPropagation();
+// }
+    let evento 
+    lista.addEventListener("dragstart", function(){
+        evento = 
+    })
